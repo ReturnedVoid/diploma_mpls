cc43aaba7552af404eb294a22fd0ba2719461f81
ReturnedVoid <andrewnech@gmail.com>
2018-12-12 16:30:35 +0200

    dont need to compile model here

--- diploma_mpls/runneural.py
+++ diploma_mpls/runneural.py
@@ -1,16 +1,12 @@
 from keras.models import load_model
 import numpy as np
 import data
-import keras
-from keras import losses
 
 X, Y = data.load_samples()
 
 loaded_model = load_model('model.h5')
 
-loaded_model.compile(optimizer=keras.optimizers.Adam(lr=0.001),
-                     loss=losses.mean_squared_error,
-                     metrics=['accuracy'])
+
 scores = loaded_model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (loaded_model.metrics_names[1], scores[1]*100))
 
