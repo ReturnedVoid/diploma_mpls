969d46f9abd589d763c3389bd4c2fd91ea7cb9ec
ReturnedVoid <andrewnech@gmail.com>
2018-12-26 03:21:57 +0200

    new softmax nn config

--- diploma_mpls/neural.py
+++ diploma_mpls/neural.py
@@ -5,31 +5,26 @@ from keras import losses
 import keras
 import matplotlib.pyplot as plt
 import data
-from project_folders import SAMPLES_PATH, MODELS_PATH
+from project_folders import MODELS_PATH
 
 np.random.seed(7)
-
-dataset = np.loadtxt(
-    '{}/dataset.csv'.format(SAMPLES_PATH), delimiter=",")
-# split into input (X) and output (Y) variables
-np.random.shuffle(dataset)
 
 X, Y = data.load_samples()
 
 # create model
 model = Sequential()
-model.add(Dense(36, input_dim=data.num_of_inputs, activation='relu'))
-model.add(Dense(150, activation='relu'))
-model.add(Dense(150, activation='relu'))
+model.add(Dense(16, input_dim=data.num_of_inputs, activation='relu'))
+model.add(Dense(12, activation='softmax'))
+model.add(Dense(8, activation='softmax'))
 model.add(Dense(data.num_of_outputs, activation='relu'))
 
 # Compile model
-model.compile(optimizer=keras.optimizers.Adam(lr=0.001),
+model.compile(optimizer=keras.optimizers.Adam(lr=0.0001),
               loss=losses.mean_squared_error,
               metrics=['accuracy'])
 
 # Fit the model
-history = model.fit(X, Y, validation_split=0.2, epochs=100, batch_size=32)
+history = model.fit(X, Y, validation_split=0.2, epochs=520, batch_size=32)
 
 
 # evaluate the model
